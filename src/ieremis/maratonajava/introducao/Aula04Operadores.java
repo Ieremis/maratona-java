f60dc42cefb8d828522246fcc5a3029a2408a064
Ieremis <ieremisgabriel@hotmail.com>
2024-12-17 14:02:16 -0300

    continuacao da aula 04

--- src/ieremis/maratonajava/introducao/Aula04Operadores.java
+++ src/ieremis/maratonajava/introducao/Aula04Operadores.java
@@ -1,4 +1,17 @@
 package ieremis.maratonajava.introducao;
 
 public class Aula04Operadores {
+    public static void main(String[] args) {
+        int numero01 = 10;
+        int numero02 = 20;
+
+        double resultado = numero01 / numero02;
+        System.out.println(resultado); //como as duas variaveis utilizadas sao int, mesmo o resultado sendo double, sera calculado com int
+
+        double numero03 = 10;
+        int numero04 = 20;
+
+        double resultado02 = numero03 / numero04;
+        System.out.println(resultado02);
+    }
 }
