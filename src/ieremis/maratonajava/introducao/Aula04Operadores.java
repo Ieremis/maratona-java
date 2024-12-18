f4a4cbbe1c7bb51a80e8374ca89496367a356080
Ieremis <ieremisgabriel@hotmail.com>
2024-12-18 15:46:17 -0300

    commit aula 05

--- src/ieremis/maratonajava/introducao/Aula04Operadores.java
+++ src/ieremis/maratonajava/introducao/Aula04Operadores.java
@@ -7,18 +7,18 @@ public class Aula04Operadores {
         int numero02 = 20;
 
         double resultadoComInt = numero01 / numero02;
-        System.out.println("resultadoComInt " + resultadoComInt); //como as duas variaveis utilizadas sao int, mesmo o resultado sendo double, sera calculado com int
+        //System.out.println("resultadoComInt " + resultadoComInt); //como as duas variaveis utilizadas sao int, mesmo o resultado sendo double, sera calculado com int
 
         double numero03 = 10;
         int numero04 = 20;
 
         double resultadoComDouble = numero03 / numero04;
-        System.out.println("resultadoComDouble " + resultadoComDouble); //imprime corretamente, mesmo somente uma variavel sendo double
+        //System.out.println("resultadoComDouble " + resultadoComDouble); //imprime corretamente, mesmo somente uma variavel sendo double
 
 
         //%
         int resto = 20 % 2; //% é um operador para RESTO
-        System.out.println("resto " + resto);
+        //System.out.println("resto " + resto);
 
         //< > <= >= == !=
         boolean isDezMaiorQueVinte = 10 > 20;
@@ -26,16 +26,47 @@ public class Aula04Operadores {
         boolean isDezIgualAVinte = 10 == 20;
         boolean isDezIgualADez = 10 == 10;
         boolean isDezDiferenteDeDez = 10 != 10;
-        System.out.println("isDezMaiorQueVinte " + isDezMaiorQueVinte);
-        System.out.println("isDezMenorQueVinte " + isDezMenorQueVinte);
-        System.out.println("isDezIgualAVinte " + isDezIgualAVinte);
-        System.out.println("isDezIgualADez " + isDezIgualADez);
-        System.out.println("isDezDiferenteDeDez " + isDezDiferenteDeDez);
+        //System.out.println("isDezMaiorQueVinte " + isDezMaiorQueVinte);
+        //System.out.println("isDezMenorQueVinte " + isDezMenorQueVinte);
+        //System.out.println("isDezIgualAVinte " + isDezIgualAVinte);
+        //System.out.println("isDezIgualADez " + isDezIgualADez);
+        //System.out.println("isDezDiferenteDeDez " + isDezDiferenteDeDez);
 
         //&& AND
-        int idadeAtual = 22;
+        int idadeAtual = 17;
         boolean isAdulto = idadeAtual >= 18 && idadeAtual < 60;
-        System.out.println("isAdulto " + isAdulto);
+        //System.out.println("isAdulto " + isAdulto);
+
+        //|| OR
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 5000;
+        float valorVideogame = 3500f;
+        boolean isVideogameAcessivel = valorTotalContaCorrente > valorVideogame || valorTotalContaPoupanca > valorVideogame;
+        //System.out.println("isVideogameAcessivel " + isVideogameAcessivel);
+
+        //= += -= *= /= %=
+        double bonus = 1800;
+        bonus = 2000;
+        //System.out.println(bonus);
+
+        bonus += 1000;
+        //System.out.println(bonus);
+
+        bonus -= 1000;
+        //System.out.println(bonus);
+
+        bonus *= 2;
+        //System.out.println(bonus);
+
+        bonus /= 2;
+        //System.out.println(bonus);
+
+        bonus %= 2;
+        //System.out.println(bonus);
+
+        int contador = 0;
+        System.out.println(contador++); //primeiro ele printa o 0 e depois incrementa
+        System.out.println(++contador); //primeiro ele incrementa o valor, e depois printa
 
     }
 }
---
+++ src/ieremis/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,15 @@
+package ieremis.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main(String[] args){
+        int idade = 17;
+        boolean isMaiorDeIdade = idade >= 18;
+
+        if(isMaiorDeIdade) {
+            System.out.println("É maior de idade");
+        }
+        else {
+            System.out.println("É menor de idade");
+        }
+    }
+}
